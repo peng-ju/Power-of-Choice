b0e2aeef4c382ac32e59e9913144e7d27c9634ab
Gautam Choudhary <gc.iitr@gmail.com>
2023-11-07 20:39:08 -0500

    starting to update base reference: modified model weights get/set/aggregate methods for generic model implementation
    Former-commit-id: 23b7dfb585f9b3015fd3b5965e6b5c72f20d4d07

---
+++ dnn/logistic_implementation/FedAvg.py
@@ -0,0 +1,187 @@
+import numpy as np
+import torch
+
+from utils import read_data
+
+class FedAvg(object):
+    def __init__(self, lr, bs, le, algo, powd, train_data_dir, test_data_dir, sample_ratio, num_classes=10, device=None):
+        """ initialize federated optimizer """
+        # hyperparameters
+        self.lr = lr  # learning rate
+        self.bs = bs  # batch size
+        self.le = le  # local epochs
+        self.algo = algo  # client selection algorithm
+        self.powd = powd  # d (power of choice param)
+        self.sample_ratio = sample_ratio  # clients per round, m
+        self.num_classes = num_classes  # number of classes in the dataset
+
+        # read data
+        _, _, self.train_data, self.test_data = read_data(train_data_dir, test_data_dir)
+        self.num_clients = len(self.train_data.keys())  # number of clients, K
+        self.ratio = self.get_ratio()  # ratio, p_k for each client k
+        self.dim = np.array(self.train_data['f_00000']['x']).shape[1]  # input dimension
+        
+        # defining the model: here, logistic regression
+        self.model = torch.nn.Linear(self.dim, self.num_classes, bias=False)
+        
+        # defining loss function: here, cross-entropy
+        self.criterion = torch.nn.CrossEntropyLoss()
+
+        # defining the optimizer: here, SGD
+        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=self.lr, weight_decay=10e-4)
+
+        # intial global params for server (similar behavior if initialized to random values)
+        self.global_parameters = []
+        with torch.no_grad():
+            for param in self.model.parameters():
+                self.global_parameters.append(torch.zeros_like(param))
+
+    def set_params(self, parameters):
+        """ set parameters of global model """
+        with torch.no_grad():
+            for model_param, param in zip(self.model.parameters(), parameters):
+                model_param.copy_(param)
+    
+    def get_params(self):
+        """ get parameters of global model """
+        local_parameters = []
+        with torch.no_grad():
+            for param in self.model.parameters():
+                local_parameters.append(param.detach().clone())
+        return local_parameters
+
+
+    def get_ratio(self):
+        """ given the datasize of each client, compute the ratio p_k = |D_k|/|D| """
+        total_size = 0
+        ratios = np.zeros(self.num_clients)
+        for i in range(self.num_clients):
+            key = 'f_{0:05d}'.format(i)
+            local_size = np.array(self.train_data[key]['x']).shape[0]
+            ratios[i] = local_size
+            total_size += local_size
+
+        return ratios/total_size
+
+    def loss(self, i):
+        """ compute loss for client `i` """
+        # fetch data for client `i`
+        uname = 'f_{0:05d}'.format(i) 
+        X = torch.tensor(self.train_data[uname]['x'], dtype=torch.float32)
+        y = torch.tensor(self.train_data[uname]['y'], dtype=torch.int64)
+
+        # foward pass
+        outputs = self.model(X)
+        # torch.nn.functional.softmax(outputs) == softmax(X@w)
+
+        # compute loss
+        loss = self.criterion(outputs, y)
+
+        return loss.item()
+
+    def evaluate(self):
+        """ evaluate global loss and local losses for all clients
+
+        local losses: loss for each client
+        global loss: average of all local client losses weighted by ratio p_k
+        """
+        global_loss = 0
+        local_losses = []
+
+        # send global parameters to all clients
+        self.set_params(self.global_parameters)
+
+        # compute loss for each client
+        for i in range(self.num_clients):
+            loss = self.loss(i)
+            global_loss += loss * self.ratio[i]
+            local_losses.append(loss)
+
+        return global_loss, local_losses
+
+    def sgd_step(self, i):
+        """ run one step of SGD on client `i` and updates model parameters """
+        # fetch data for client `i`
+        uname = 'f_{0:05d}'.format(i) 
+        X = torch.tensor(self.train_data[uname]['x'], dtype=torch.float32)
+        y = torch.tensor(self.train_data[uname]['y'], dtype=torch.int64)
+
+        # fetch mini-batch (stochasticity)
+        sample_idx = np.random.choice(X.shape[0], size=self.bs)
+
+        # zero the gradients
+        self.optimizer.zero_grad()
+
+        # forward pass
+        outputs = self.model(X[sample_idx])
+
+        # compute loss
+        loss = self.criterion(outputs, y[sample_idx])
+
+        # backward pass - compute gradients
+        loss.backward()
+
+        # backward pass - update weights
+        self.optimizer.step()
+        return
+
+    def local_update(self, active_clients):
+        """ train the set of active clients """
+        client_params = []
+        for i in active_clients:
+            # send global parameters to client `i`
+            self.set_params(self.global_parameters)
+            
+            # run E steps of SGD on client `i`
+            for _ in range(self.le):
+                self.sgd_step(i)
+            
+            # get local parameters from client `i`
+            local_parameters = self.get_params()
+            client_params.append(local_parameters)
+
+        return client_params, active_clients
+
+    def aggregate(self, client_params):
+        """ aggregation strategy in FedAvg """
+        with torch.no_grad():
+            # zero out the global parameters for accumulation
+            for param in self.global_parameters:
+                param.zero_()
+            
+            # sum local parameters
+            for local_params in client_params:
+                for global_param, param in zip(self.global_parameters, local_params):
+                    global_param.add_(param)
+                    # option 2: similar behaviour but slightly different
+                    # global_param.add_(param, alpha=1/len(client_params))
+            
+            # average local parameters
+            for global_param in self.global_parameters:
+                global_param.div_(len(client_params))
+
+
+    def select_client(self, loc_loss):
+        """ client selection strategy for each round of communication """
+        if not loc_loss:
+            # For the first round, select 'm' clients uniformly at random
+            idxs_users = np.random.choice(self.num_clients, size=self.sample_ratio, replace=False)
+
+        else:
+            if self.algo == 'rand':
+                # Step 1: select 'm' clients with probability proportional to their loss with replacement
+                idxs_users = np.random.choice(self.num_clients, p=self.ratio, size=self.sample_ratio, replace=True)
+
+            elif self.algo == 'pow-d' or self.algo == 'adapow-d':
+                # Step 1: select 'd' clients with probability proportional to their loss without replacement
+                rnd_idx = np.random.choice(self.num_clients, p=self.ratio, size=self.powd, replace=False)
+                
+                # Step 2: sort the selected clients in descending order of their loss
+                repval = list(zip([loc_loss[i] for i in rnd_idx], rnd_idx))
+                repval.sort(key=lambda x: x[0], reverse=True)
+                rep = list(zip(*repval))
+                
+                # Step 3: select indices of top 'm' clients from the sorted list
+                idxs_users = rep[1][:int(self.sample_ratio)]
+
+        return idxs_users
---
+++ dnn/logistic_implementation/main.py
@@ -0,0 +1,171 @@
+import os
+import json
+from tqdm import tqdm
+import numpy as np
+import argparse
+
+from matplotlib import rcParams
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+from FedAvg import FedAvg
+
+## hyperparameters
+train_data_dir = './synthetic_data/'
+test_data_dir = './synthetic_data/'
+lr = 0.05  # learning rate, \eta
+bs = 50  # batch size, b
+le = 30  # local epoch, E
+total_rnd = 100  # total communication rounds, T/E
+clients_per_round = 3  # active clients per round, m
+K = 30  # number of clients, K
+SEED = 12345
+log_remote = False
+
+## create logs directory if not exist
+if not os.path.exists('./logs'):
+    os.makedirs('./logs')
+
+## plot settings
+# color maps reference: https://matplotlib.org/stable/users/explain/colors/colormaps.html#qualitative
+# line styles reference: https://matplotlib.org/stable/gallery/lines_bars_and_markers/linestyles.html
+c_t = cm.get_cmap('tab10')
+ftsize = 16
+params = {'legend.fontsize': ftsize,
+         'axes.labelsize': ftsize,
+         'axes.titlesize':ftsize,
+         'xtick.labelsize':ftsize,
+         'ytick.labelsize':ftsize}
+plt.rcParams.update(params)
+lw = 2
+plt.rcParams["font.family"] = "Times New Roman"
+plt.rcParams['axes.labelweight'] = 'bold'
+# plt.figure(figsize=(16,14.5))
+plt.subplots_adjust(right=1.1, top=0.9)
+rcParams['axes.titlepad'] = 14
+
+## experiment configurations
+# key=experiment_id, value=(algo, powd, color, linestyle)
+client_selection_type = {
+    'rand': ('rand', 1, 'k', '-'),
+    'powd2': ('pow-d', clients_per_round*2, c_t(3), '-.'),
+    'powd5': ('pow-d', clients_per_round*10, c_t(0), '--'),
+    # 'adapow30': ('adapow-d', K, c_t(1), (0, (5, 10)))
+}
+
+def args_parser():
+    """ parse command line arguments """
+
+    # basic parameters
+    parser = argparse.ArgumentParser(description="FMNIST baseline")
+    parser.add_argument("--name", "-n", default="default", type=str, help="experiment name, used for saving results")
+    parser.add_argument("--model", default="MLP", type=str, help="neural network model")
+    parser.add_argument("--dataset", default="fmnist", type=str, help="type of dataset")
+    parser.add_argument("--num_classes", type=int, default=10, help="number of classes")
+
+    # hyperparams
+    parser.add_argument("--algo", default="rand", type=str, help="type of client selection ($\pi$)")
+    parser.add_argument("--num_clients", default=100, type=int, help="total number of clients, $K$")
+    parser.add_argument("--rounds", default=500, type=int, help="total communication rounds")
+    parser.add_argument("--clients_per_round", default=1, type=int, help="number of local workers")
+    parser.add_argument("--localE", default=30, type=int, help="number of local epochs, $E$")
+    parser.add_argument("--constantE", action="store_true", help="whether all the local workers have an identical number of local epochs or not")
+    parser.add_argument("--bs", default=64, type=int, help="batch size on each worker/client, $b$")
+    parser.add_argument("--lr", default=0.1, type=float, help="client learning rate, $\eta$")
+    parser.add_argument("--decay", default=1, type=bool, help="1: decay LR, 0: no decay")
+    parser.add_argument("--alpha", default=0.2, type=float, help="control the non-iidness of dataset")
+    parser.add_argument("--NIID", action="store_true", help="whether the dataset is non-iid or not")
+
+    # algo specific hyperparams
+    parser.add_argument("--powd", default=6, type=int, help="number of selected subset workers per round ($d$)")
+    parser.add_argument("--commE", action="store_true", help="activation of $cpow-d$")
+    parser.add_argument("--momentum", default=0.0, type=float, help="local (client) momentum factor")
+    parser.add_argument("--mu", default=0, type=float, help="mu parameter in fedprox")
+    parser.add_argument("--gmf", default=0, type=float, help="global (server) momentum factor")
+    parser.add_argument("--rnd_ratio", default=0.1, type=float, help="hyperparameter for afl")
+    parser.add_argument("--delete_ratio", default=0.75, type=float, help="hyperparameter for afl")
+
+    # logistics
+    parser.add_argument("--print_freq", default=100, type=int, help="print info frequency")
+    parser.add_argument("--num_workers", default=0, type=int, help="number of workers for data loading")
+    parser.add_argument("--seed", default=1, type=int, help="random seed")
+    parser.add_argument("--save", "-s", action="store_true", help="whether save the training results")
+    parser.add_argument("--p", "-p", action="store_true", help="whether the dataset is partitioned or not")
+    
+    # distributed setup
+    parser.add_argument("--backend", default="gloo", type=str, help="backend name")
+    parser.add_argument("--world_size", default=1, type=int, help="world size for distributed training")
+    parser.add_argument("--rank", default=0, type=int, help="the rank of worker")
+    parser.add_argument("--initmethod", default="tcp://", type=str, help="init method")
+
+    args = parser.parse_args()
+    return args
+
+def main(args):
+    ## run experiments
+    for key in client_selection_type.keys():
+        # set seed for reproducibility
+        np.random.seed(SEED)
+
+        # fetch configuration
+        algo, powd, color, lstyle = client_selection_type[key]
+
+        # run federated learning experiment for given configuration
+        server = FedAvg(lr, bs, le, algo, powd, train_data_dir, test_data_dir, clients_per_round)
+        errors, local_losses = [], []
+        for rnd in tqdm(range(total_rnd), desc=key): 
+            # (optional) decay learning rate according to round index
+            # reduce learning rate by half after 300 and 600 rounds
+            if args.decay == True:
+                # update_learning_rate(optimizer, rnd, args.lr)
+                if rnd == 300 or rnd == 600:
+                    for param_group in server.optimizer.param_groups:
+                        param_group["lr"] /= 2
+                        
+            # reduce powd from K to m after half rounds (only for 'adapow-d')
+            if algo == 'adapow-d' and rnd == total_rnd//2:
+                server.powd = clients_per_round
+
+            # find the set of active clients
+            active_clients = server.select_client(local_losses)
+
+            # train active clients locally
+            weights, _ = server.local_update(active_clients)
+
+            # update global parameter by aggregating weights
+            server.aggregate(weights)
+
+            # evaluate global and local losses
+            global_loss, local_losses = server.evaluate()
+            errors.append(global_loss)
+
+        # save errors to json file
+        with open(f'./logs/m={clients_per_round}_algo={key}_errors.json', 'w') as f:
+            json.dump(errors, f)
+        
+        # # load errors from json file
+        # with open(f'./logs/m={clients_per_round}_algo={key}_errors.json') as f:
+        #     errors = json.load(f)
+        
+        # plot global loss for each configuration
+        if algo =='rand' or algo =='adapow-d':
+            p_label = algo
+        else:
+            p_label = algo+', d={}'.format(powd)
+        plt.plot(errors, lw=lw, color=color, ls = lstyle, label=p_label)
+
+    # update plot settings
+    plt.ylabel('Global loss')
+    plt.xlabel('Communication round')
+    plt.xticks()
+    plt.yticks()
+    plt.legend(loc=1)
+    plt.grid()
+    plt.title('K=30, m={}'.format(clients_per_round))
+    # plt.show()
+    plt.savefig(f'synthetic_m={clients_per_round}.pdf', bbox_inches='tight')
+    print(f'saving plot to synthetic_m={clients_per_round}.pdf')
+
+if __name__ == '__main__':
+    args = args_parser()
+    main(args)
