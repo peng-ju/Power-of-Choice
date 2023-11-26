6f9b7b149f420bc6ac9cbb0cc1fbf70f2e110566
Gautam Choudhary <gc.iitr@gmail.com>
2023-11-26 06:24:36 -0500

    added launch.sh and minor update in gitignore
    Former-commit-id: e76c130ea5a99e0deeac71815d947b052b3d88f4

---
+++ dnn/launch.sh
@@ -0,0 +1,20 @@
+python main.py --config configs/fig3a.json
+python main.py --config configs/fig3b.json
+
+python main.py --config configs/fig4a.json
+python main.py --config configs/fig4b.json
+
+python main.py --config configs/fig5a.json
+python main.py --config configs/fig5b.json
+
+python main.py --config configs/fig13a.json
+python main.py --config configs/fig13b.json
+
+python main.py --config configs/fig14a.json
+python main.py --config configs/fig14b.json
+
+python main.py --config configs/fig15a.json
+python main.py --config configs/fig15b.json
+
+python main.py --config configs/fig16a.json
+python main.py --config configs/fig16b.json
